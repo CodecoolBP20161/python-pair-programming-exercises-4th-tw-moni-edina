65d3ae7ec844e2adb682ef22bc17333f2d7932ed
beru26 <berkes.edina@gmail.com>
2016-06-22 09:45:20 +0200

    Person is OK

--- 3-phone-numbers/person.py
+++ 3-phone-numbers/person.py
@@ -3,18 +3,18 @@ class Person():
     _phone_number = None
 
     def __init__(self, name, phone_number):
-        # implent this method
-        pass  # delete this
+        self._name = name
+        self._phone_number = phone_number
 
     def is_phone_number_matching(self, input_phone_number):
-        # implent this method
-        pass  # delete this
+        return self._phone_number == input_phone_number
 
     def get_name(self):
-        # implent this method
-        pass  # delete this
+        if self.is_phone_number_matching:
+            return self._name
+        else:
+            return "This number is not matching."
 
     @staticmethod
     def normalize_phone_number(phone_number):
-        # implent this method
-        pass  # delete this
+        return phone_number.replace("-", "").replace(" ", "")
